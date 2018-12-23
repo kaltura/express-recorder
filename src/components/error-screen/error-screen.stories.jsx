55a51847040d826b31c307489533b423198ead0b
Atar Shadmi <atar.shadmi@kaltura.com>
2018-12-23 14:19:25 +0200

    error screens component, design, and stories

---
+++ src/components/error-screen/error-screen.stories.jsx
@@ -0,0 +1,11 @@
+ import { h } from "preact";
+import { storiesOf } from "@storybook/react";
+import {ErrorScreen} from "./error-screen";
+
+
+storiesOf("Error Screen", module)
+    .add("render an error screen without title", () => (<ErrorScreen text={'A terrible error has occured'} ></ErrorScreen>))
+    .add("render an error screen with title", () => (<ErrorScreen title={'Oh, No!'} text={'A terrible error has occured'} ></ErrorScreen>))
+    .add("render an error screen with HTML text", () => (<ErrorScreen title={'Oh, No!'}
+                                                                      text={'A terrible <a href="http://google.com">error</a> has occured'} ></ErrorScreen>))
+    ;
---
+++ src/components/error-screen/error-screen.tsx
@@ -0,0 +1,32 @@
+import {Component, h} from "preact";
+const styles = require('./style.scss');
+
+type Props = {
+    title?: string;
+    text: string;
+}
+
+type State = {
+
+}
+
+
+export class ErrorScreen extends Component<Props, State>{
+
+    render(props:Props) {
+        const {title, text} = props;
+
+        return (
+            <div class={`error-screen ${styles['error-screen-wrap']}`}>
+                <div className={`${styles['error-screen']}`}>
+                    {title && <h1 class={`error-screen__title ${styles['error-screen__title']}`}>
+                        {title}
+                    </h1>}
+                    <p class={`error-screen__text ${styles['error-screen__text']}`} dangerouslySetInnerHTML={{__html: text}}>
+                    </p>
+                </div>
+            </div>
+);
+
+    }
+}
