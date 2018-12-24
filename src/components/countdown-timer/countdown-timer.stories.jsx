1aab3e1ee673f2c4412beaa5a089c251b9fe9360
Atar Shadmi <atar.shadmi@kaltura.com>
2018-12-24 08:33:57 +0200

    only set dark BG for this story

--- src/components/countdown-timer/countdown-timer.stories.jsx
+++ src/components/countdown-timer/countdown-timer.stories.jsx
@@ -3,7 +3,7 @@ import { storiesOf } from "@storybook/react";
 import {CountdownTimer} from "./CountdownTimer";
 
 storiesOf("Countdown Timer", module)
+    .addDecorator(story => <div style={{ backgroundColor: '#333', width: '100%', height:'100%' }}>{story()}</div>)
     .add("render a countdown timer", () => {
-        document.getElementsByTagName('body')[0].style.backgroundColor = "#333";
         return <CountdownTimer initialValue={3} onCountdownComplete={() => console.log("done")}/>
     } );
