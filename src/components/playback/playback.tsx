39e384ef0b2550d6bf93590ec611fc5eb56455ae
Channa Korach <channa53@gmail.com>
2018-12-30 19:55:37 +0200

    KMS-18807 - playback component - fixed

--- src/components/playback/playback.tsx
+++ src/components/playback/playback.tsx
@@ -1,51 +1,56 @@
-import {Component, h} from "preact";
-const styles = require('./style.scss');
+import { Component, h } from "preact";
+const styles = require("./style.scss");
+declare var KalturaPlayer: any;
 
 type Props = {
-    media: {blob:Blob, mimeType:string},
-    partnerId: number,
-    uiconfId: number
-}
+    media: { blob: Blob; mimeType: string };
+    partnerId: number;
+    uiconfId: number;
+};
 
-type State = {
+type State = {};
 
-}
-
-
-export class Playback extends Component<Props, State>{
-
-    kalturaPlayer: any;  // can't type as KalturaPlayer
+export class Playback extends Component<Props, State> {
+    kalturaPlayer: any;
 
     componentDidMount(): void {
         this.embedPlayer();
     }
 
-    componentDidUpdate(previousProps: Props, previousState: State, previousContext: any): void {
-        const {media} = this.props;
+    componentDidUpdate(
+        previousProps: Props,
+        previousState: State,
+        previousContext: any
+    ): void {
+        const { media } = this.props;
         if (previousProps.media !== media) {
             // play the new media
             this.setMedia(media);
         }
     }
 
-
-    setMedia(media: {blob:Blob, mimeType:string}) {
-        this.kalturaPlayer.setMedia({    /* note - not loadMedia() */
+    setMedia(media: { blob: Blob; mimeType: string }) {
+        this.kalturaPlayer.setMedia({
             sources: {
-                progressive: [{
-                    url: window.URL.createObjectURL(media.blob)/* create url from the blob */,
-                    mimetype: media.mimeType /* probably "video/webm", whatever we create */
-                }]
+                progressive: [
+                    {
+                        url: window.URL.createObjectURL(
+                            media.blob
+                        ),
+                        mimetype:
+                            media.mimeType
+                    }
+                ]
             },
             plugins: {}
-        })
+        });
     }
 
     embedPlayer() {
-        const {media, partnerId, uiconfId} = this.props;
+        const { media, partnerId, uiconfId } = this.props;
         try {
             this.kalturaPlayer = KalturaPlayer.setup({
-                targetId: 'player-wrap',
+                targetId: "player-wrap",
                 provider: {
                     partnerId: partnerId,
                     uiConfId: uiconfId
@@ -53,14 +58,16 @@ export class Playback extends Component<Props, State>{
             });
             this.setMedia(media);
         } catch (e) {
-            console.error(e.message)
+            console.error(e.message);
         }
     }
 
-
-    render(props:Props) {
+    render(props: Props) {
         return (
-            <div id={'player-wrap'} class={`player-wrap ${styles['player-wrap']}`}></div>
+            <div
+                id={"player-wrap"}
+                class={`player-wrap ${styles["player-wrap"]}`}
+            />
         );
     }
 }
