275c34bdff1c705b044efae77723e5d732470f17
Channa Korach <channa53@gmail.com>
2018-12-30 10:22:15 +0200

    KMS-18807 - prettier

--- src/components/recording-timer/RecordingTimer.tsx
+++ src/components/recording-timer/RecordingTimer.tsx
@@ -1,38 +1,35 @@
-import {Component, h} from "preact";
+import { Component, h } from "preact";
 
-const styles = require('./style.scss');
-const iconStyles  = require( '../../styles.scss');
+const styles = require("./style.scss");
+const iconStyles = require("../../styles.scss");
 
 type Props = {
     onButtonClick: () => void;
-}
+};
 
 type State = {
-    currentTime:number; // in seconds
-    clickedOnce:boolean;
-}
+    currentTime: number; // in seconds
+    clickedOnce: boolean;
+};
 
+export class RecordingTimer extends Component<Props, State> {
+    interval: number | undefined;
 
-export class RecordingTimer extends Component<Props, State>{
-
-    interval:number | undefined;
-
-
-    constructor(props:Props) {
+    constructor(props: Props) {
         super(props);
-        this.state = { currentTime: 0 , clickedOnce: false};
+        this.state = { currentTime: 0, clickedOnce: false };
         this.clickHandler = this.clickHandler.bind(this);
     }
 
     componentDidMount() {
-        const int:any = setInterval(() => {
+        const int: any = setInterval(() => {
             this.update();
         }, 1000);
         this.interval = int as number;
     }
 
     update() {
-        this.setState({currentTime: this.state.currentTime + 1});
+        this.setState({ currentTime: this.state.currentTime + 1 });
     }
 
     clickHandler() {
@@ -41,22 +38,26 @@ export class RecordingTimer extends Component<Props, State>{
         }
 
         clearInterval(this.interval);
-        this.setState({clickedOnce: true}, () => {
+        this.setState({ clickedOnce: true }, () => {
             if (this.props.onButtonClick) {
                 this.props.onButtonClick();
             }
-        })
+        });
     }
 
-    render(props:Props, state:State) {
-        const {currentTime} = state;
+    render(props: Props, state: State) {
+        const { currentTime } = state;
         return (
-            <div className={`timer ${styles['timer']}`}>
-                <button type={"button"} class={`timer-button ${styles['timer-button']}`} onClick={this.clickHandler}>
-                <span className={iconStyles['icon-stop']}>
-                    <span className={iconStyles['path1']}></span>
-                    <span className={iconStyles['path2']}></span>
-                </span>
+            <div className={`timer ${styles["timer"]}`}>
+                <button
+                    type={"button"}
+                    class={`timer-button ${styles["timer-button"]}`}
+                    onClick={this.clickHandler}
+                >
+                    <span className={iconStyles["icon-stop"]}>
+                        <span className={iconStyles["path1"]} />
+                        <span className={iconStyles["path2"]} />
+                    </span>
                     {currentTime}
                 </button>
             </div>
