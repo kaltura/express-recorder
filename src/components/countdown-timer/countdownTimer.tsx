31c47d245be3f0c009d1ed90876647d0601d43f4
alon ainbinder <alon.ainbinder@kaltura.com>
2019-01-15 19:31:49 +0200

    KMS-18962 changing class to be more specific

--- src/components/countdown-timer/countdownTimer.tsx
+++ src/components/countdown-timer/countdownTimer.tsx
@@ -57,10 +57,11 @@ export class CountdownTimer extends Component<Props, State> {
         const { countdown } = state;
         const { radius } = props;
 
+
         return (
             <div class={`countdown ${styles["countdown"]}`}>
                 <div className={`countdown-number ${styles["countdown-number"]}`}>{countdown}</div>
-                <svg className={styles["svg"]}>
+                <svg className={styles["svg-countDownTimer"]}>
                     <circle
                         className={styles["circle"]}
                         ref={circle => (this.circleRef = circle as HTMLElement)}
