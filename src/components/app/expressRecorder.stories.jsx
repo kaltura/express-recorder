1c00ac8e2189b19aa35c104f903e1edd20bd1fdd
alon1980 <alon.ainbinder@kaltura.com>
2019-02-18 15:36:15 +0200

    Adding KS and details of demo partner so storybook will work out of the box

--- src/components/app/expressRecorder.stories.jsx
+++ src/components/app/expressRecorder.stories.jsx
@@ -6,12 +6,12 @@ storiesOf("Main App", module).add("widget default", () => {
     return (
         <div>
             <ExpressRecorder
-                ks={""}
-				app={""}
-                serviceUrl={""}
-				partnerId={}
-				uiConfId={}
-				playerUrl={""}
+               ks={"djJ8MjQ4Mzg0MXwFX0AgrmUwhGdtU7R9u2xnSaKZNf3drr6sOfsYBR_x4y1gCSF7-MZnUmPE0m_rGxe_321u9LNVLFX5OMqOhvdWcGm53V80wT0KDONPaXxl5w=="}
+				app={"local"}
+                serviceUrl={"https://www.kaltura.com"}
+				partnerId={2483841}
+				uiConfId={43735261}
+				playerUrl={"https://www.kaltura.com"}
             />
         </div>
     );
