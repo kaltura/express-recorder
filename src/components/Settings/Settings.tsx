bd6edd7b1791f98b56da26d55500e5acbd3f8d30
odedmo <odedmoran222@gmail.com>
2022-01-30 08:23:06 +0200

    KMS-21629 - use SR only elements

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -312,7 +312,6 @@ export class Settings extends Component<Props, State> {
                                 aria-labelledby="dropdownMenu"
                             >
                                 <a
-                                    aria-label={translator.translate("Camera Settings")}
                                     className={styles["resource-link"]}
                                     onClick={() => {
                                         this.getResourceSettings(ResourceTypes.VIDEO);
@@ -322,8 +321,14 @@ export class Settings extends Component<Props, State> {
                                     }}
                                     tabIndex={0}
                                 >
+                                    <span className={styles["sr-only"]}>
+                                        {translator.translate("Camera Settings")}
+                                    </span>
                                     <div className={styles["resources-item"]}>
-                                        <div className={styles["resources-name"]}>
+                                        <div
+                                            className={styles["resources-name"]}
+                                            aria-hidden="true"
+                                        >
                                             {translator.translate("Camera")}
                                         </div>
                                         <div className={styles["resource-label"]}>
@@ -335,7 +340,6 @@ export class Settings extends Component<Props, State> {
                                     </div>
                                 </a>
                                 <a
-                                    aria-label={translator.translate("Audio Settings")}
                                     className={styles["resource-link"]}
                                     onClick={() => {
                                         this.getResourceSettings(ResourceTypes.AUDIO);
@@ -345,9 +349,14 @@ export class Settings extends Component<Props, State> {
                                     }}
                                     tabIndex={0}
                                 >
+                                    <span className={styles["sr-only"]}>
+                                        {translator.translate("Audio Settings")}
+                                    </span>
                                     <div className={styles["resources-item"]}>
                                         <div className={styles["resources-name"]}>
-                                            {translator.translate("Audio")}
+                                            <span aria-hidden="true">
+                                                {translator.translate("Audio")}
+                                            </span>
                                             {stream && (
                                                 <div className={styles["settings-audio-indicator"]}>
                                                     <AudioIndicator
