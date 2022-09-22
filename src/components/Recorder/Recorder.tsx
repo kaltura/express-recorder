89b66d6bf49aa4d0c3ade4584e364e17641e01d8
Channa Korach Cohen <channa.korachcohen@kaltura.com>
2022-09-22 13:03:54 +0300

    ILMS-279 - rename function

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -46,12 +46,12 @@ export class Recorder extends Component<Props, State> {
         this.startTime = 0;
     }
     componentDidMount() {
-        this.startStream();
+        this.startStreamIfPossible();
     }
 
     componentDidUpdate(prevProps: Props) {
         const { doRecording, discard } = this.props;
-        this.startStream();
+        this.startStreamIfPossible();
 
         if (doRecording !== prevProps.doRecording) {
             this.toggleRecording();
@@ -63,7 +63,7 @@ export class Recorder extends Component<Props, State> {
         }
     }
 
-    startStream = () => {
+    startStreamIfPossible = () => {
         const { videoStream, screenStream, doPlayback } = this.props;
         if (!doPlayback) {
             if (this.videoRef && videoStream) {
