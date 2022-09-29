303f3c3f823f5bd4d025d4152d9c9ead05405251
Channa Korach Cohen <channa.korachcohen@kaltura.com>
2022-09-29 15:06:22 +0300

    ILMS-302 - add reset button to error screen

--- src/components/App/ExpressRecorder.tsx
+++ src/components/App/ExpressRecorder.tsx
@@ -330,7 +330,8 @@ export class ExpressRecorder extends Component<ExpressRecorderProps, State> {
                 recordedBlobs: [],
                 screenRecordedBlobs: [],
                 doPlayback: false,
-                error: ""
+                error: "",
+                shareScreenOn: false
             },
             () => {
                 this.stopStreams();
@@ -379,7 +380,7 @@ export class ExpressRecorder extends Component<ExpressRecorderProps, State> {
 
         const isProcessing = () => !!(processingCamera || processingScreen);
 
-        this.setState({ processing: true });
+        this.setState({ processing: isProcessing() });
         if (recordedBlobs.length) {
             const blob = new Blob(recordedBlobs, { type: "video/webm" });
             // handle chrome blob duration issue
@@ -731,7 +732,7 @@ export class ExpressRecorder extends Component<ExpressRecorderProps, State> {
         if (error !== "") {
             return (
                 <div className={`express-recorder ${styles["express-recorder"]}`}>
-                    <ErrorScreen text={error} />
+                    <ErrorScreen text={error} onReset={this.resetApp} />
                 </div>
             );
         }
--- src/components/Error-screen/ErrorScreen.tsx
+++ src/components/Error-screen/ErrorScreen.tsx
@@ -1,25 +1,34 @@
 import { Component, h } from "preact";
 const styles = require("./style.scss");
+import { Translator } from "../Translator/Translator";
 
 type Props = {
     title?: string;
     text: string;
+    onReset: () => void;
 };
 
 export function ErrorScreen(props: Props) {
-    const { title, text } = props;
+    const { title, text, onReset } = props;
+    const translator = Translator.getTranslator();
     return (
-        <div class={`xr_error-screen ${styles["error-screen-wrap"]}`}>
+        <div className={`xr_error-screen ${styles["error-screen-wrap"]}`}>
             <div className={`${styles["error-screen"]}`}>
                 {title && (
-                    <h1 class={`xr_error-screen__title ${styles["error-screen__title"]}`}>
+                    <h1 className={`xr_error-screen__title ${styles["error-screen__title"]}`}>
                         {title}
                     </h1>
                 )}
                 <p
-                    class={`xr_error-screen__text ${styles["error-screen__text"]}`}
+                    className={`xr_error-screen__text ${styles["error-screen__text"]}`}
                     dangerouslySetInnerHTML={{ __html: text }}
                 />
+                <button
+                    className={`xr_error-screen__reset-button ${styles["error-screen__reset-button"]}`}
+                    onClick={onReset}
+                >
+                    {translator.translate("Reset App")}
+                </button>
             </div>
         </div>
     );
